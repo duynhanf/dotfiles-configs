d45f8f3dd5f9d6a27e5f4bc21a37ab7c3542c292
duynhanf <duynhanf@gmail.com>
2023-08-05 10:26:23 +0700

    add setup mac script

---
+++ setup_mac.sh
@@ -0,0 +1,36 @@
+# !/bin/sh
+
+echo "> Installing Homebrew"
+# curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh
+
+brew -v
+
+echo "> Installing Git"
+# brew install git
+
+echo "> Installing VSCode"
+# brew install --cask visual-studio-code
+
+echo "> Installing Google Chrome"
+# brew install --cask google-chome
+
+echo "> Installing Slack"
+# brew install --cask slack
+
+echo "> Installing Node"
+# brew install node
+
+echo "> Installing Go"
+# brew install go
+
+echo "> Installing kubectl"
+# brew install kubernetes-cli
+
+echo "> Installing helm"
+# brew install helm
+
+echo "> Installing Terraform"
+# brew tap hashicorp/tap
+# brew install hashicorp/tap/terraform
+
+
