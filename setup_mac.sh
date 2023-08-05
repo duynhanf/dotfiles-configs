a2b40ff38cad4c2e32d6d9a2f19974059217fe5c
duynhanf <duynhanf@gmail.com>
2023-08-05 11:05:52 +0700

    update

--- setup_mac.sh
+++ setup_mac.sh
@@ -1,36 +1,45 @@
 # !/bin/sh
 
-echo "> Installing Homebrew"
+# /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/duynhanf/dotfiles-configs/HEAD/setup_mac.sh)"
+
+G='\033[0;32m'
+N='\033[0m' # No Color
+
+echo "${G}> Installing Homebrew${N}"
 # curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh
 
 brew -v
 
-echo "> Installing Git"
+echo "${G}> Installing Git${N}"
 # brew install git
 
-echo "> Installing VSCode"
+echo "${G}> Installing VSCode${N}"
 # brew install --cask visual-studio-code
 
-echo "> Installing Google Chrome"
+echo "${G}> Installing Google Chrome${N}"
 # brew install --cask google-chome
 
-echo "> Installing Slack"
+echo "${G}> Installing Slack${N}"
 # brew install --cask slack
 
-echo "> Installing Node"
+echo "${G}> Installing Node${N}"
 # brew install node
 
-echo "> Installing Go"
+echo "${G}> Installing Go${N}"
 # brew install go
 
-echo "> Installing kubectl"
+echo "${G}> Installing kubectl${N}"
 # brew install kubernetes-cli
 
-echo "> Installing helm"
+echo "${G}> Installing helm${N}"
 # brew install helm
 
-echo "> Installing Terraform"
+echo "${G}> Installing Terraform${N}"
 # brew tap hashicorp/tap
 # brew install hashicorp/tap/terraform
 
+echo "${G}> Installing pnpm${N}"
+# brew install pnpm
 
+echo "${G}> Installing Neovim${N}"
+# brew install neovim
